f097ccc85942cb72fd2476762670e3b4860a8690
tiagorahal <rahal.aires@gmail.com>
2022-02-08 08:00:41 -0300

    Create business class

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -9,5 +9,34 @@ const business = {
   zipCode: '10101',
   category: 'Italian',
   rating: 4.5,
-  reviewCount: 90
+  reviewCount: 90,
 };
+
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg" alt="" />
+        </div>
+        <h2>{business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{business.address}</p>
+            <p>{business.state}</p>
+            <p>{business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{business.category}</h3>
+            <h3 className="rating">
+              {business.rating}
+              {' '}
+              stars
+            </h3>
+            <p>{business.reviewCount}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
